c9f90737670e8082e475dc41c2fdbadb417016c5
David Battefeld <davidbattefeld@gmail.com>
2023-07-19 19:20:03 +0200

    feat: add syntax highlighting lib

--- src/components/CodeBlock.svelte
+++ src/components/CodeBlock.svelte
@@ -1,5 +1,12 @@
+<script>
+    import hljs from 'highlight.js'
+
+    //TODO: make this work
+    hljs.highlightAll()
+</script>
+
 <div class="mockup-code">
     <pre data-prefix="$"><code>npm i daisyui</code></pre> 
-    <pre data-prefix=">" class="text-warning"><code>installing...</code></pre> 
-    <pre data-prefix=">" class="text-success"><code>Done!</code></pre>
+    <pre data-prefix=">"><code>installing...</code></pre> 
+    <pre data-prefix=">"><code>Done!</code></pre>
 </div>
--- src/components/CodeExampleSection.svelte
+++ src/components/CodeExampleSection.svelte
@@ -5,7 +5,7 @@
 
 <div class="card lg:card-side w-full bg-base-200 border-2 border-secondary shadow-xl my-4">
     <div class="card-body">
-        <div class="grid grid-cols-2">
+        <div class="grid grid-cols-1 md:grid-cols-2">
             <div>
                 <h2 class="card-title">Languages</h2>
                 <p class="my-3">that I use and sometimes love</p>
