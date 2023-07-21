64218c1e2a1f4cd5b346275525d9b72985ab1927
Obnoxieux <davidbattefeld@gmail.com>
2023-07-21 19:15:20 +0200

    feat: add first draft of Projects view

---
+++ src/components/projects/TechStackSection.svelte
@@ -0,0 +1,21 @@
+<div class="prose">
+    <h2>My TechStack</h2>
+    <p>Text</p>
+</div>
+<div class="grid grid-cols-1 md:grid-cols-2 gap-6 justify-items-center my-4">
+    <div class="card card-compact w-full bg-base-200 border-2">
+        <figure><img class="max-w-[50%] mt-3" src="/static/swiftui-og.png" alt="SwiftUI logo" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                SwiftUI
+            </h2>
+            <p>Declarative UI framework that allows the creation of apps for all Apple platforms with maximum code reusability.</p>
+            <div class="card-actions justify-end mt-2">
+                <div class="badge badge-outline">macOS</div>
+                <div class="badge badge-outline">iOS</div>
+                <div class="badge badge-outline">watchOS</div>
+                <div class="badge badge-outline">tvOS</div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,4 +1,11 @@
+<script>
+    import TechStackSection from "../../components/projects/TechStackSection.svelte";
+</script>
+
 <div class="prose">
-    <h1>Projects</h1>
+    <h1>My Work</h1>
     <p>This is the project page.</p>
 </div>
+<div class="divider"></div>
+<TechStackSection/>
+<div class="divider"></div>
