a7f869ca8102982515bedeaef12fedc9ce18452b
Obnoxieux <davidbattefeld@gmail.com>
2023-07-22 19:56:35 +0200

    feat: complete project tech stack section

--- src/components/projects/TechStackSection.svelte
+++ src/components/projects/TechStackSection.svelte
@@ -1,10 +1,14 @@
-<div class="prose">
-    <h2>My TechStack</h2>
-    <p>Text</p>
-</div>
-<div class="grid grid-cols-1 md:grid-cols-2 gap-6 justify-items-center my-4">
-    <div class="card card-compact w-full bg-base-200 border-2">
-        <figure><img class="max-w-[50%] mt-3" src="/static/swiftui-og.png" alt="SwiftUI logo" /></figure>
+<section id="tech-stack" class="mb-4 mt-2 flex flex-col gap-2">
+    <h2 class="font-semibold mb-2 text-xl text-accent">My TechStack</h2>
+    <p class="text-3xl sm:text-4xl font-extrabold">Building UI. One step at a time.</p>
+    <div class="mt-4 max-w-3xl space-y-6">
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.</p>
+    </div>
+</section>
+<div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6 justify-items-center my-4">
+    <div class="card card-compact w-full bg-base-200 border-2 border-blue-500">
+        <figure><img class="max-w-[50%] mt-5" src="/swiftui-og.png" alt="SwiftUI logo" /></figure>
         <div class="card-body">
             <h2 class="card-title">
                 SwiftUI
@@ -15,6 +19,43 @@
                 <div class="badge badge-outline">iOS</div>
                 <div class="badge badge-outline">watchOS</div>
                 <div class="badge badge-outline">tvOS</div>
+            </div>
+        </div>
+    </div>
+    <div class="card card-compact w-full bg-base-200 border-2 border-emerald-400">
+        <figure><img class="max-w-[45%] mt-5" src="/jetpack_compose_icon.png" alt="Jetpack Compose logo" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                Jetpack Compose
+            </h2>
+            <p>The modern declarative UI toolkit by Google, mostly for Android, but also usable for cross-platform desktop apps.</p>
+            <div class="card-actions justify-end mt-2">
+                <div class="badge badge-outline">Android</div>
+                <div class="badge badge-outline">Desktop</div>
+            </div>
+        </div>
+    </div>
+    <div class="card card-compact w-full bg-base-200 border-2 border-orange-600">
+        <figure><img class="max-w-[40%] mt-6" src="/Svelte_Logo.svg" alt="Svelte logo" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                Svelte
+            </h2>
+            <p>Framework for JavaScript / TypeScript for beautiful and fast web applications that run in every modern browser.</p>
+            <div class="card-actions justify-end mt-2">
+                <div class="badge badge-outline">Web</div>
+            </div>
+        </div>
+    </div>
+    <div class="card card-compact w-full bg-base-200 border-2 border-amber-500">
+        <figure><img class="max-w-[50%] mt-5" src="/TYPO3-Logo.svg" alt="TYPO3 logo" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                TYPO3
+            </h2>
+            <p>The world's leading enterprise CMS, suitable for every website ranging from small representations for individuals to full-scale multilingual content hubs.</p>
+            <div class="card-actions justify-end mt-2">
+                <div class="badge badge-outline">Web</div>
             </div>
         </div>
     </div>
