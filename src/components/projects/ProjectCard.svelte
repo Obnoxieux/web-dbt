53bc86f0f2e0d9cd2597424cb2f578bd6675917f
Obnoxieux <davidbattefeld@gmail.com>
2023-08-10 23:42:44 +0200

    feat: begin styling project page

---
+++ src/components/projects/ProjectCard.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import type {GitHubRepo} from "$lib/model/GitHubRepo";
+
+    export let repo: GitHubRepo
+
+    function determineColor(language: string): string {
+        switch (language) {
+            case "Swift":
+                return "swift"
+            default:
+                return "secondary"
+        }
+    }
+</script>
+
+<div class="card w-full bg-base-200 border-2 border-neutral-content shadow-xl">
+    <div class="card-body">
+        <h2 class="card-title">{repo.name}</h2>
+        <p>{repo.description}</p>
+        <div class="card-actions justify-between items-center">
+            <div class="badge badge-lg badge-outline badge-{determineColor(repo.language)}">{repo.language}</div>
+            <a href="#" class="btn btn-outline btn-accent">Details</a>
+        </div>
+    </div>
+</div>
--- src/routes/mywork/projects/+page.svelte
+++ src/routes/mywork/projects/+page.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
+    import ProjectCard from "../../../components/projects/ProjectCard.svelte";
+
     export let data;
 </script>
 
-<h2>{data.repos[0].name}</h2>
-<p>{data.repos[0].language}</p>
+<h1 class="h1 mb-5 lg:mb-8">My Projects</h1>
+<div class="flex flex-wrap mb-5 md:mb-10 gap-2 md:gap-4 items-center">
+    <div class="h2">Filter by Language:</div>
+    {#each data.repos as repo}
+        <button class="btn btn-sm md:btn-md btn-outline">{repo.language}</button>
+    {/each}
+</div>
+<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 lg:gap-8 xl:gap-9">
+    {#each data.repos as repo}
+        <ProjectCard {repo}/>
+    {/each}
+</div>
