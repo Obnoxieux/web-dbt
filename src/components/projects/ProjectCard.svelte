b68f4dadf39252f1113df4e8a262c59af1912dec
David Battefeld <davidbattefeld@gmail.com>
2023-08-13 13:24:33 +0200

    feat: make language filters work

--- src/components/projects/ProjectCard.svelte
+++ src/components/projects/ProjectCard.svelte
@@ -1,22 +1,8 @@
 <script lang="ts">
     import type {GitHubRepo} from "$lib/model/GitHubRepo";
+    import {ColorUtility} from "$lib/classes/ColorUtility";
 
     export let repo: GitHubRepo
-
-    function determineColor(language: string): string {
-        switch (language) {
-            case "Swift":
-                return "swift"
-            case "Kotlin":
-                return "kotlin"
-            case "Svelte":
-                return "typescript"
-            case "TypeScript":
-                return "typescript"
-            default:
-                return "neutral"
-        }
-    }
 </script>
 
 <div class="card w-full bg-base-200 border-2 border-neutral-content shadow-xl">
@@ -24,7 +10,7 @@
         <h2 class="card-title">{repo.name}</h2>
         <p>{repo.description}</p>
         <div class="card-actions justify-between items-center">
-            <div class="badge badge-lg badge-outline badge-{determineColor(repo.language)}">{repo.language}</div>
+            <div class="badge badge-lg badge-outline badge-{ColorUtility.determineColor(repo.language)}">{repo.language}</div>
             <a href="#" class="btn btn-outline">Details</a>
         </div>
     </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -3,23 +3,42 @@
     import type {GitHubRepo} from "$lib/model/GitHubRepo";
 
     export let data;
+    let shownLanguage: string = "All"
 
-    let languages: Array<string> = []
+    let languages: Array<string> = ["All"]
     data.repos.forEach((repo: GitHubRepo) => {
         languages.push(repo.language)
     })
-    const filters = new Set<string>(languages)
+    let filters = new Set<string>(languages)
+
+    let filteredRepos: GitHubRepo[] = data.repos
+
+    function toggleFilter(filter: string) {
+        shownLanguage = filter
+        if (filter === "All") {
+            filteredRepos = data.repos
+        } else {
+            filteredRepos = data.repos.filter((repo: GitHubRepo) => repo.language === filter)
+        }
+    }
 </script>
 
 <h1 class="h1 mb-5 lg:mb-8">My Projects</h1>
 <div class="flex flex-wrap mb-5 md:mb-10 gap-2 md:gap-4 items-center">
     <div class="h2">Filter by Language:</div>
     {#each filters as filter}
-        <button class="btn btn-sm md:btn-md btn-outline">{filter}</button>
+        <button
+            on:click={() => toggleFilter(filter)}
+            class="filter-button btn btn-sm md:btn-md"
+            class:btn-neutral={filter === shownLanguage}
+            class:btn-outline={filter !== shownLanguage}
+        >
+            {filter}
+        </button>
     {/each}
 </div>
 <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 lg:gap-8 xl:gap-9">
-    {#each data.repos as repo}
+    {#each filteredRepos as repo}
         <ProjectCard {repo}/>
     {/each}
 </div>
