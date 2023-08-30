37af951bdd8729235fb9238e05675af57edae3ed
David Battefeld <davidbattefeld@gmail.com>
2023-08-30 23:53:23 +0200

    feat: add transition to CodeBlock.svelte

--- src/components/home/CodeBlock.svelte
+++ src/components/home/CodeBlock.svelte
@@ -2,6 +2,8 @@
   import Highlight, { LineNumbers } from "svelte-highlight";
   import githubDark from "svelte-highlight/styles/github-dark";
   import { languages } from "$lib/codeSamples";
+  import {cubicIn} from "svelte/easing";
+  import { fly } from 'svelte/transition';
 
   export let lang = languages.typescript
 </script>
@@ -10,8 +12,10 @@
   {@html githubDark}
 </svelte:head>
 
-<div class="mockup-code col-span-2">
-    <Highlight language={lang.languageType} code={lang.codeSample} let:highlighted>
-        <LineNumbers {highlighted} />
-    </Highlight>
-</div>
+{#key lang}
+    <div in:fly={{x: -50, easing: cubicIn}} class="mockup-code col-span-2 min-h-[640px]">
+        <Highlight language={lang.languageType} code={lang.codeSample} let:highlighted>
+            <LineNumbers {highlighted} />
+        </Highlight>
+    </div>
+{/key}
