abc94eaed6da723b0a6ec34a44e1f769821cb58e
David Battefeld <davidbattefeld@gmail.com>
2023-08-30 23:23:28 +0200

    feat: add transition to stats section

--- src/components/personal/BaseballStatsSection.svelte
+++ src/components/personal/BaseballStatsSection.svelte
@@ -6,6 +6,9 @@
     import PitchingStats from "./PitchingStats.svelte";
     import type {FieldingStatisticsEntry} from "$lib/model/FieldingStatisticsEntry";
     import FieldingStats from "./FieldingStats.svelte";
+    import { slide } from 'svelte/transition';
+    import { fade } from 'svelte/transition';
+    import {cubicIn, cubicOut} from "svelte/easing";
 
     export let statsBatting: BattingStatisticsEntry;
     export let statsPitching: PitchingStatisticsEntry;
@@ -18,26 +21,31 @@
     }
 </script>
 
-<div class="flex flex-col gap-10">
-    <div class="max-w-3xl space-y-6">
-        <h2 class="h2 mb-2">My Stats</h2>
-        <p>Baseball is a sport all about numbers. These are mine for the current season. Data is imported automatically via the <a class="link" href="https://bsm.baseball-softball.de/api_docs" target="_blank">API of the German Baseball & Softball Association</a>.</p>
-    </div>
-    <div class="tabs tabs-boxed justify-around my-2 shadow-md">
-        <button on:click={() => changeStatsTab(StatsType.batting)} class="tab grow {activeStatsTab === StatsType.batting ? 'tab-active' : ''}">Batting</button>
-        <button on:click={() => changeStatsTab(StatsType.pitching)} class="tab grow {activeStatsTab === StatsType.pitching ? 'tab-active' : ''}">Pitching</button>
-        <button on:click={() => changeStatsTab(StatsType.fielding)} class="tab grow {activeStatsTab === StatsType.fielding ? 'tab-active' : ''}">Fielding</button>
-    </div>
+<div class="max-w-xs sm:max-w-sm md:max-w-4xl lg:max-w-full">
+    <div class="flex flex-col gap-10">
+        <div class="max-w-3xl space-y-6">
+            <h2 class="h2 mb-2">My Stats</h2>
+            <p>Baseball is a sport all about numbers. These are mine for the current season. Data is imported automatically via the <a class="link" href="https://bsm.baseball-softball.de/api_docs" target="_blank">API of the German Baseball & Softball Association</a>.</p>
+        </div>
+        <div class="tabs tabs-boxed justify-around my-2 shadow-md">
+            <button on:click={() => changeStatsTab(StatsType.batting)} class="tab grow {activeStatsTab === StatsType.batting ? 'tab-active' : ''}">Batting</button>
+            <button on:click={() => changeStatsTab(StatsType.pitching)} class="tab grow {activeStatsTab === StatsType.pitching ? 'tab-active' : ''}">Pitching</button>
+            <button on:click={() => changeStatsTab(StatsType.fielding)} class="tab grow {activeStatsTab === StatsType.fielding ? 'tab-active' : ''}">Fielding</button>
+        </div>
 
-    {#if activeStatsTab === StatsType.batting}
-        <BattingStats {statsBatting}/>
-    {:else if activeStatsTab === StatsType.pitching}
-        <PitchingStats {statsPitching}/>
-    {:else}
-        <FieldingStats {statsFielding}/>
-    {/if}
+        {#key activeStatsTab}
+        <div in:slide={{delay: 150, easing: cubicIn}} out:fade={{duration: 150, easing: cubicOut}} class="flex flex-col gap-10">
+            {#if activeStatsTab === StatsType.batting}
+                    <BattingStats {statsBatting}/>
+            {:else if activeStatsTab === StatsType.pitching}
+                    <PitchingStats {statsPitching}/>
+            {:else}
+                    <FieldingStats {statsFielding}/>
+            {/if}
+        </div>
+        {/key}
+    </div>
 </div>
-
 
 <style>
     .tabs-boxed .tab-active:not(.tab-disabled):not([disabled]) {
