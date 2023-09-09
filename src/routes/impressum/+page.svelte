61de7f51432271abcbf19f74ea319995ebff232e
David Battefeld <davidbattefeld@gmail.com>
2023-09-09 13:12:50 +0200

    feat: add legal docs and corresponding keys to .env
    closes #9

--- src/routes/impressum/+page.svelte
+++ src/routes/impressum/+page.svelte
@@ -1,1 +1,17 @@
-<h1 class="h1">Impressum</h1>
+<script lang="ts">
+    import {PUBLIC_CONTACT_ADDRESS, PUBLIC_CONTACT_EMAIL_ADDRESS, PUBLIC_CONTACT_PHONE} from "$env/static/public";
+    const contactEmailAddress = PUBLIC_CONTACT_EMAIL_ADDRESS
+    const contactPhone = PUBLIC_CONTACT_PHONE
+    const contactAddress = PUBLIC_CONTACT_ADDRESS
+</script>
+
+<div class="prose">
+    <h1>Impressum</h1>
+    <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+    <p>David Battefeld<br />
+        {contactAddress}</p>
+    <h2>Kontakt</h2>
+    <p>Telefon: {contactPhone}<br />
+        E-Mail: <a href="mailto:{contactEmailAddress}">{contactEmailAddress}</a></p>
+    <p>Quelle: <a href="https://www.e-recht24.de">https://www.e-recht24.de</a></p>
+</div>
