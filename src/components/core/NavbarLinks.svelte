0ecfbbbf451d2496e1ae993449610a0b3a566870
David Battefeld <davidbattefeld@gmail.com>
2023-09-10 11:50:25 +0200

    perf: add preloading

--- src/components/core/NavbarLinks.svelte
+++ src/components/core/NavbarLinks.svelte
@@ -1,5 +1,5 @@
 <li><a class="text-xl" href="/">Home</a></li>
 <li><a class="text-xl" href="/tech">Tech Stack</a></li>
-<li><a class="text-xl" href="/projects">Projects</a></li>
-<li><a class="text-xl" href="/personal">Personal</a></li>
+<li><a class="text-xl" href="/projects" data-sveltekit-preload-data>Projects</a></li>
+<li><a class="text-xl" href="/personal" data-sveltekit-preload-data>Personal</a></li>
 <li><a class="text-xl" href="/contact">Contact</a></li>
--- src/components/projects/ProjectCard.svelte
+++ src/components/projects/ProjectCard.svelte
@@ -12,13 +12,13 @@
      */
 </script>
 
-<div class="card w-full h-full bg-base-200 border-2 border-neutral-content shadow-xl">
+<div class="card w-full h-full bg-base-200 border-2 border-neutral-content shadow-xl hover:scale-105">
     <div class="card-body">
         <h2 class="card-title">{repo.name}</h2>
         <p>{repo.description}</p>
         <div class="card-actions justify-between items-center">
             <div class="badge badge-lg badge-{color} dark:badge-outline">{repo.language}</div>
-            <a href="projects/{slug}" class="btn btn-outline" data-sveltekit-preload-code>Details</a>
+            <a href="projects/{slug}" class="btn btn-outline" data-sveltekit-preload-data>Details</a>
         </div>
     </div>
 </div>
