023833ea023b1e69da44f2ae6913487d91e6f283
David Battefeld <davidbattefeld@gmail.com>
2023-09-18 22:23:16 +0200

    fix: do not show homepage on null

--- src/components/projects/ProjectDetailLinkSection.svelte
+++ src/components/projects/ProjectDetailLinkSection.svelte
@@ -8,7 +8,7 @@
 <section>
     <h2 class="subheader col-span-4 text-{color} mb-3">Links</h2>
     <div class="col-span-4 flex gap-2 md:gap-4">
-        {#if repo.homepage !== ""}
+        {#if repo.homepage !== "" && repo.homepage !== null}
             <a href="{repo.homepage}" target="_blank" role="button" class="btn btn-neutral">Homepage</a>
         {/if}
         <a href="{repo.html_url}" target="_blank" role="button" class="btn btn-neutral">Repository</a>
