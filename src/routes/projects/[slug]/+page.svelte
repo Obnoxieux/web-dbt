b6fd0e62f7f27374a4d1fd3fff239bd44ca6a1dd
David Battefeld <davidbattefeld@gmail.com>
2023-09-24 18:29:26 +0200

    fix: add flex container also to project detail
    hopefully last visual glitch

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -8,10 +8,12 @@
     const color = ColorUtility.determineColor(data.repo.language)
 </script>
 
-<div class="flex flex-col gap-3">
-    <ProjectDetailIntro repo="{data.repo}"/>
-    <div class="divider"></div>
-    <ProjectDetailMainContainer repo="{data.repo}" {color}/>
-    <div class="divider col-span-4"></div>
-    <ProjectDetailLinkSection repo="{data.repo}" {color}/>
+<div class="flex flex-col min-w-full md:w-screen max-w-[90%] lg:max-w-[70%]">
+    <div class="flex flex-col gap-3">
+        <ProjectDetailIntro repo="{data.repo}"/>
+        <div class="divider"></div>
+        <ProjectDetailMainContainer repo="{data.repo}" {color}/>
+        <div class="divider col-span-4"></div>
+        <ProjectDetailLinkSection repo="{data.repo}" {color}/>
+    </div>
 </div>
