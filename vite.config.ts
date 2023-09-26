3144b69a91f0059bdc6078e1ec15b164e8bfe6d5
David Battefeld <davidbattefeld@gmail.com>
2023-09-26 18:58:19 +0200

    feat: add touch icons in various formats

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import {sveltekit} from '@sveltejs/kit/vite';
+import {defineConfig} from 'vite';
 
 export default defineConfig({
 	server: {
+		host: true,
 		fs: {
 			// Allow serving files from one level up to the project root
 			allow: ['..'],
