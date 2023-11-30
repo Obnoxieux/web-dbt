2c623797501f32e162097f3a44ca443db2d6b9b7
David Battefeld <davidbattefeld@gmail.com>
2023-12-01 00:11:05 +0100

    docs: add basic routing for app privacy section

---
+++ src/routes/projects/apps/+page.svelte
@@ -0,0 +1,9 @@
+<div class="flex flex-col min-w-full md:w-screen max-w-[90%] lg:max-w-[70%]">
+    <div class="prose">
+        <h1>Apps</h1>
+        <p>This section contains links some mandatory legal and marketing info for App Store and Google Play Pages for
+            my custom apps. It might get more fancy styling later.
+        </p>
+        <a href="/projects/apps/germansucks">German Sucks (Android)</a>
+    </div>
+</div>
---
+++ src/routes/projects/apps/germansucks/privacy/+page.svelte
@@ -0,0 +1,5 @@
+<div class="flex flex-col min-w-full md:w-screen max-w-[90%] lg:max-w-[70%]">
+    <div class="prose">
+        TO BE WRITTEN
+    </div>
+</div>
