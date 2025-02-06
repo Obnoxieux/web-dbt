0d9b8547cb5a8f6b9ad2658cd8d30ab274ff8725
David Battefeld <davidbattefeld@gmail.com>
2025-02-07 00:30:19 +0100

    build: configure for monorepo

--- website/src/routes/+page.ts
+++ website/src/routes/+page.ts
@@ -1,13 +1,13 @@
-import { OpenGraphDefaults } from '$lib/classes/OpenGraphDefaults';
-import type { MetaTagsProps } from 'svelte-meta-tags';
+import {OpenGraphDefaults} from '$lib/classes/OpenGraphDefaults';
+import type {MetaTagsProps} from 'svelte-meta-tags';
 
-export const load = async ({ url }) => {
-    const pageTitle = 'Main'
+export const load = async ({url}) => {
+  const pageTitle = 'Main'
 
-    const metaTags: MetaTagsProps = Object.freeze({
-        title: pageTitle,
-        openGraph: OpenGraphDefaults.getOpenGraphObject(url, pageTitle, OpenGraphDefaults.description),
-    });
+  const metaTags: MetaTagsProps = Object.freeze({
+    title: pageTitle,
+    openGraph: OpenGraphDefaults.getOpenGraphObject(url, pageTitle, OpenGraphDefaults.description),
+  });
 
   return {
     metaTagsChild: metaTags
